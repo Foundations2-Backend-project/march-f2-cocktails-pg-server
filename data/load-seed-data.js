b9b0990cd61d140b72865eb25d77c276b7e86fbe
kayla schoorl <kaylaschoorl@gmail.com>
2021-05-25 15:30:34 -0700

    fixed eslint errors

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -26,7 +26,7 @@ async function run() {
     await Promise.all(
       cocktails.map(cocktail => {
         return client.query(`
-        INSERT INTO cocktails (name, category, alcohol_present, glass, instructions, image, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient8, ingredient9, ingredient10, ingredient11, ingredient12, ingredient13, ingredient14, ingredient15, user_id)
+        INSERT INTO favorites (name, category, alcohol_present, glass, instructions, image, ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient8, ingredient9, ingredient10, ingredient11, ingredient12, ingredient13, ingredient14, ingredient15, user_id)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22)
         `,
         [cocktail.name, cocktail.category, cocktail.alcohol_present, cocktail.glass, cocktail.instructions, cocktail.image, cocktail.ingredient1, cocktail.ingredient2, cocktail.ingredient3, cocktail.ingredient4, cocktail.ingredient5, cocktail.ingredient6, cocktail.ingredient7, cocktail.ingredient8, cocktail.ingredient9, cocktail.ingredient10, cocktail.ingredient11, cocktail.ingredient12, cocktail.ingredient13, cocktail.ingredient14, cocktail.ingredient15, user.id]);
